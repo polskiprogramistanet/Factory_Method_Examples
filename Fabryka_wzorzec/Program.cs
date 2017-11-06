2d4b9821916ecddbacbb5b22d79252059c8aa49e
polskiprogramistanet <rmbas@DESKTOP-DVCMQO5>
2017-11-06 12:05:05 +0100

    Add project files.

---
+++ Fabryka_wzorzec/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka_wzorzec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
